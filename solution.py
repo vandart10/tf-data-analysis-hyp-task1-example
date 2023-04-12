a25e2b76bd0658cdf276582a92b4539de4c29165
vandart10 <73397775+vandart10@users.noreply.github.com>
2023-04-12 21:32:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 710820274 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
+def solution(x_success: int,
+             x_cnt: int,
+             y_success: int,
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    conv1 = x_success / x_cnt
+    conv2 = y_success / y_cnt
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+    z = (conv1 - conv2) / np.sqrt(p * (1-p) * (1/x_cnt + 1/y_cnt))
+
+    return abs(z) > stats.norm.ppf(0.03)
