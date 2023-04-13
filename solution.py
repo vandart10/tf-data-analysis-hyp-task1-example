fd875486c27cf4dc499b7db245a88fb68b6f94b9
vandart10 <73397775+vandart10@users.noreply.github.com>
2023-04-13 23:20:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
+from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 710820274 # Ваш chat ID, не меняйте название переменной
 
@@ -8,9 +8,5 @@ def solution(x_success: int,
              x_cnt: int,
              y_success: int,
              y_cnt: int) -> bool:
-    conv1 = x_success / x_cnt
-    conv2 = y_success / y_cnt
-    p = (x_success + y_success) / (x_cnt + y_cnt)
-    z = (conv1 - conv2) / np.sqrt(p * (1-p) * (1/x_cnt + 1/y_cnt))
-
-    return abs(z) > stats.norm.ppf(0.015)
+    _, p_value = statsmodels.stats.proportion.proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+    return p_value < 0.06
